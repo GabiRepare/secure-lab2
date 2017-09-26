1b1c8f27cf5398863695b627c383895cd1411bfd
Juleinn <juleintartem@hotmail.fr>
2017-09-26 16:06:58 -0400

    Added K computation (to be modified)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
+from random import randint
+import math
 app = Flask(__name__)
+
+#db = ['Alice', ]
+
+g = 13
+p = 541 
 
 @app.route('/')
 def hello_world():
@@ -8,4 +15,9 @@ def hello_world():
 @app.route("/submit", methods=['POST'])
 def submit():
 	print(request.form['username'])
+	# generate 
+	b = randint(1, 80)
+	beta = math.pow(g, b) % p
+	print("beta = ", int(beta))
+
 	return "OK"
